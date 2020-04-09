9fa2b243ba4b8e8c868049b8a88feef77e226b82
Sofia Hu <56163482+sofia-hu@users.noreply.github.com>
2020-04-09 11:14:52 -0400

    0409

---
+++ js/main.js
@@ -0,0 +1,46 @@
+/* =====================
+Leaflet Configuration
+===================== */
+
+var map = L.map('map', {
+  center: [25.779867, -80.223289],
+  zoom: 11
+});
+var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  subdomains: 'abcd',
+  minZoom: 0,
+  maxZoom: 20,
+  ext: 'png'
+}).addTo(map);
+
+var dataset = "data/ppr_districts_2018.geojson"
+var featureGroup;
+
+var myStyle = function(feature) {
+  var collColor;
+  if (feature["properties"]["POP2010"]>3000){
+    collColor = 'red'
+  };
+  return {fillColor: collColor};
+};
+
+var myFilter = function(feature) {
+  if(feature["properties"]["POP2010"]<3000){
+      return false;
+  }
+  else{
+    return true;
+  };
+};
+
+$(document).ready(function() {
+  $.ajax(dataset).done(function(data) {
+    var parsedData = JSON.parse(data);
+    featureGroup = L.geoJson(parsedData,{
+      //style: myStyle,
+      //filter: myFilter
+    }
+  ).addTo(map);
+  });
+});
