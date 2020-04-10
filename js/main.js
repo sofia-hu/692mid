df69be51b53575a05df57982e83f69b88f80a226
Sofia Hu <56163482+sofia-hu@users.noreply.github.com>
2020-04-10 18:02:12 -0400

    0410

--- js/main.js
+++ js/main.js
@@ -4,8 +4,9 @@ Leaflet Configuration
 
 var map = L.map('map', {
   center: [25.779867, -80.223289],
-  zoom: 11
+  zoom: 12
 });
+
 var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   subdomains: 'abcd',
@@ -14,19 +15,19 @@ var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{
   ext: 'png'
 }).addTo(map);
 
-var dataset = "https://raw.githubusercontent.com/sofia-hu/692mid/master/data/0409.json"
-var featureGroup;
+var dataset = "https://raw.githubusercontent.com/sofia-hu/692mid/master/data/cliptract.geojson"
 
 var myStyle = function(feature) {
   var collColor;
-  if (feature["properties"]["POP2010"]>3000){
-    collColor = 'red'
-  };
+  if (feature["properties"]["POP2010"]>4000){
+    collColor = 'blue'
+  }
+  else{collColor = 'lightblue'};
   return {fillColor: collColor};
 };
 
 var myFilter = function(feature) {
-  if(feature["properties"]["POP2010"]<3000){
+  if(feature["properties"]["POP2010"]<0){
       return false;
   }
   else{
@@ -34,13 +35,73 @@ var myFilter = function(feature) {
   };
 };
 
-$(document).ready(function() {
-  $.ajax(dataset).done(function(data) {
-    var parsedData = JSON.parse(data);
-    featureGroup = L.geoJson(parsedData,{
-      style: myStyle,
-      filter: myFilter
-    }
-  ).addTo(map);
-  });
-});
+var myStyle1 = function(feature) {
+  var collColor;
+  if (feature["properties"]["HU2010"]>2000){
+    collColor = 'yellow'
+  }
+  else{collColor = 'lightyellow'};
+  return {fillColor: collColor};
+};
+
+var myFilter1 = function(feature) {
+  if(feature["properties"]["HU2010"]<0){
+      return false;
+  }
+  else{
+    return true;
+  };
+};
+
+var myStyle2 = function(feature) {
+  var collColor;
+  if (feature["properties"]["WHITENH"]>600){
+    collColor = 'green'
+  };
+  //else{collColor = 'lightgreen'};
+  return {fillColor: collColor};
+};
+
+var myFilter2 = function(feature) {
+  if(feature["properties"]["WHITENH"]<600){
+      return false;
+  }
+  else{
+    return true;
+  };
+};
+
+var myStyle3 = function(feature) {
+  var collColor;
+  if (feature["properties"]["BLACKNH"]>600){
+    collColor = 'purple'
+  };
+  return {fillColor: collColor};
+};
+
+var myFilter3 = function(feature) {
+  if(feature["properties"]["BLACKNH"]<600){
+      return false;
+  }
+  else{
+    return true;
+  };
+};
+
+var myStyle4 = function(feature) {
+  var collColor;
+  if (feature["properties"]["HISPAN"]>2000){
+    collColor = 'red'
+  }
+  else{collColor = 'lightred'};
+  return {fillColor: collColor};
+};
+
+var myFilter4 = function(feature) {
+  if(feature["properties"]["HISPAN"]<0){
+      return false;
+  }
+  else{
+    return true;
+  };
+};
